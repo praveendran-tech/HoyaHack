0d8018ca983b7d062ce1e68ffe1e5b86874b0301
praveendran-tech <praveend@umd.edu>
2025-01-26 01:50:54 -0500

    fixing cors issue

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hoya-hack-k6i5.vercel.app/api", // Replace with your backend's base URL
+  baseURL: "http://hoya-hack-k6i5.vercel.app/api", // Replace with your backend's base URL
   // Do not set 'Content-Type' headers here; let the browser handle it for FormData
 });
 
